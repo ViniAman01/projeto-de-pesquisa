e1b389cbac07ff133e2a6b736d6d69126a8e86ea
ViniAman01 <vinisaman@gmail.com>
2023-11-01 15:50:29 -0400

    Adicionando extensão de teste, para pegar conteudo html de página

---
+++ extension5/popup.js
@@ -0,0 +1,16 @@
+
+document.getElementById("getHtmlButton").addEventListener("click", function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    const activeTab = tabs[0];
+    chrome.scripting.executeScript({
+      target: { tabId: activeTab.id },
+      function: getHTML,
+    });
+  });
+});
+
+function getHTML() {
+  const html = document.documentElement.outerHTML;
+  console.log(html);
+  // Você pode fazer o que quiser com o HTML aqui
+}
