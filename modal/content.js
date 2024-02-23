7214597ca623add01b223f983ef95de2184ff397
ViniAman01 <vinisaman@gmail.com>
2024-02-23 13:30:45 -0400

    Adicionando arquivos omitidos no commit anterior

--- modal/content.js
+++ modal/content.js
@@ -2,10 +2,16 @@ var modal = document.getElementById("modal");
 
 var btn = document.getElementById("myBtn");
 
+var btn_submit = document.getElementById("btn-submit")
+
 var span = document.getElementsByClassName("close")[0];
 
 btn.onclick = () => {
   modal.showModal()
+}
+
+btn_submit.onclick = () => {
+  modal.close()
 }
 
 span.onclick = () => {
