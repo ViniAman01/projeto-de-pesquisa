9497ba47000b045b6b12afaec1a33f849e404fe4
ViniAman01 <103665509+ViniAman01@users.noreply.github.com>
2024-05-16 20:56:41 -0400

    Os botões já têm funcionalidade. Porém a falta de sincronismo entre o HTML e o storage local está causando comportamentos indeterminaveis. É necessária a refatoração de diversas partes.

--- src/modal.js
+++ src/modal.js
@@ -24,47 +24,70 @@ function excluirHorarioOriginal(element){
   element.target.addEventListener("click",desfazerExclusao);
 }
 
-function excluirHorarioAdicionado(element){
+async function excluirHorarioAdicionado(element){
+  var json_dias_irregulares = (await chrome.storage.local.get(['dias_irregulares']))['dias_irregulares'];
+  const dias_irregulares = new Map(JSON.parse(json_dias_irregulares));
+  const key_dias_irregulares = element.target.parentNode.parentNode.parentNode.parentNode.firstChild.innerText.split("\n")[0];
+  const dia_e_s_array = dias_irregulares.get(key_dias_irregulares);
+  const e_s_array = dia_e_s_array[1];
+  const e_s_div_elements = element.target.parentNode.parentNode.querySelectorAll("p");
+
+  let index_horario_exclusao = 0;
+  while(e_s_div_elements[index_horario_exclusao].innerText != element.target.parentNode.firstChild.innerText){
+    index_horario_exclusao++;
+  }
+
+  e_s_array.splice(index_horario_exclusao,1);
+
+  dia_e_s_array[1] = e_s_array;
+  dias_irregulares.set(key_dias_irregulares,dia_e_s_array); 
+  json_dias_irregulares = JSON.stringify(Array.from(dias_irregulares));
+  chrome.storage.local.set({'dias_irregulares': json_dias_irregulares});
   element.target.parentNode.remove();
 }
 
-function criaDivES(e_s_array, original_bool){
+function criaES(horario,index){
+  let div_p_botao = document.createElement("div");
+
+  if(index % 2 == 0){
+    prefix = 'E:'
+  }else{
+    prefix = 'S:'
+  }
+
+  div_p_botao.insertAdjacentHTML('beforeend', '<p>' + prefix + horario + '</p>');
+
+  let botao_excluir = document.createElement("button");
+
+  botao_excluir.className = 'bt-excluir';
+
+  botao_excluir.innerText = 'Excluir Horario';
+
+  if(horario.indexOf('*') == -1){
+    botao_excluir.addEventListener("click", excluirHorarioOriginal);
+  }else{
+    botao_excluir.addEventListener("click", excluirHorarioAdicionado);
+  }
+
+  div_p_botao.insertAdjacentElement('beforeend', botao_excluir);
+
+  return div_p_botao;
+}
+
+function criaDivES(e_s_array){
   var e_s_div = document.createElement("div");
 
   e_s_array.forEach((e,index) => {
-    let div_p_botao = document.createElement("div");
-
-    if(index % 2 == 0){
-      prefix = 'E:'
-    }else{
-      prefix = 'S:'
-    }
-
-    div_p_botao.insertAdjacentHTML('beforeend', '<p>' + prefix + e + '</p>');
-
-    let botao_excluir = document.createElement("button");
-
-    botao_excluir.className = 'bt-excluir';
-
-    botao_excluir.innerText = 'Excluir Horario';
-
-    if(original_bool){
-      botao_excluir.addEventListener("click", excluirHorarioOriginal);
-    }else{
-      botao_excluir.addEventListener("click", excluirHorarioAdicionado);
-    }
-
-    div_p_botao.insertAdjacentElement('beforeend', botao_excluir);
-
+    const div_p_botao = criaES(e,index);
     e_s_div.insertAdjacentElement('beforeend', div_p_botao)
   });
 
   return e_s_div;
 }
 
-function criaCelulaES(e_s_array,e_s_celula,original_bool){
+function criaCelulaES(e_s_array,e_s_celula){
 
-  const e_s_div = criaDivES(e_s_array,original_bool);
+  const e_s_div = criaDivES(e_s_array);
 
   e_s_celula.insertAdjacentElement('beforeend', e_s_div)
 
@@ -87,32 +110,43 @@ async function adicionarHorario(element){
     const key_dias_irregulares = td.parentNode.firstChild.innerText.split("\n")[0];
     const dia_e_s_array = dias_irregulares.get(key_dias_irregulares);
     const e_s_array = dia_e_s_array[1];
-    console.log(e_s_array);
     const obj_novo_horario = new Horario(value_novo_horario,':');
+    let index_e_s_child;
 
     e_s_array.every((e,index) => {
-      let obj_aux_horario = new Horario(e,':');
+      horario = e.replace('*','');
+      let obj_aux_horario = new Horario(horario,':');
+      index_e_s_child = index;
 
       if(obj_aux_horario.ehIgual(obj_novo_horario)){
+        index_e_s_child = -1;
         return false;
       }
 
       if(obj_aux_horario.ehMaior(obj_novo_horario)){
-        e_s_array.splice(index,0,value_novo_horario);
         return false; 
       }
 
       return true;
     });
 
-    td.firstChild.remove();
-    const e_s_div = criaDivES(e_s_array,false);
-    td.insertAdjacentElement('afterbegin',e_s_div);
+    if(index_e_s_child != -1){
+      const e_s_div_childs = td.firstChild.childNodes;
+      if(index_e_s_child == e_s_array.length-1){
+        const e_s_div = criaES(value_novo_horario + '*',e_s_array.length);
+        td.firstChild.insertAdjacentElement('beforeend',e_s_div);
+      }else{
+        const e_s_div = criaES(value_novo_horario + '*',index_e_s_child);
+        e_s_div_childs[index_e_s_child].insertAdjacentElement('beforebegin',e_s_div);
+      }
 
-    dia_e_s_array[1] = e_s_array;
-    dias_irregulares.set(key_dias_irregulares,dia_e_s_array); 
-    json_dias_irregulares = JSON.stringify(Array.from(dias_irregulares));
-    chrome.storage.local.set({'dias_irregulares': json_dias_irregulares});
+      e_s_array.splice(index_e_s_child,0,value_novo_horario + '*');
+
+      dia_e_s_array[1] = e_s_array;
+      dias_irregulares.set(key_dias_irregulares,dia_e_s_array); 
+      json_dias_irregulares = JSON.stringify(Array.from(dias_irregulares));
+      chrome.storage.local.set({'dias_irregulares': json_dias_irregulares});
+    }
   }
 }
 
@@ -135,7 +169,7 @@ async function adicionarHorario(element){
 
       dia_data_string =  data + '<br>' + dia_semana;
 
-      criaCelulaES(e_s_array,e_s_celula,true);
+      criaCelulaES(e_s_array,e_s_celula);
 
       dia_horarios_regulares_string = '';
       dia_horarios_regulares.forEach(e => {
